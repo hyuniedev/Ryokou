6f80b3105cc88f9e5b772d98a8b2f413076fcae4
NguyenHuuNien <huunien734@gmail.com>
2024-08-23 18:01:07 +0700

    + add time change page on slider

--- ryokou/lib/ui/slider/slider.dart
+++ ryokou/lib/ui/slider/slider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class SliderBar extends StatefulWidget {
@@ -8,11 +10,43 @@ class SliderBar extends StatefulWidget {
 }
 
 class _Slider extends State<SliderBar> {
+  late PageController _pageController;
+  late Timer _timer;
+  int _curPage = 0;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _curPage);
+    _timer = Timer.periodic(const Duration(seconds: 4), (Timer timer) {
+      if (_curPage < widget.numSlider - 1) {
+        _curPage++;
+      } else {
+        _curPage = 0;
+      }
+      _pageController.animateToPage(
+        _curPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.ease,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 150,
       child: PageView(
+        controller: _pageController,
+        onPageChanged: (value) {
+          _curPage = value;
+        },
         scrollDirection: Axis.horizontal,
         children: List.generate(widget.numSlider,
             (index) => ItemSlider(index, MediaQuery.of(context).size.width)),
