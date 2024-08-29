2985adf20f744d8f4e9db8200d52d1d64a7a030d
NguyenHuuNien <huunien734@gmail.com>
2024-08-29 12:55:23 +0700

    + add boxSuggest

--- ryokou/lib/ui/appbar/top_app_bar.dart
+++ ryokou/lib/ui/appbar/top_app_bar.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ryokou/themes/colors_theme.dart';
 
-// class GetAppBar extends StatelessWidget implements PreferredSizeWidget {
-//   final Widget childAppBar;
-//   const GetAppBar({super.key, required this.childAppBar});
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//   }
-
-//   @override
-//   Size get preferredSize => const Size.fromHeight(100.0);
-// }
-
 PreferredSizeWidget getAppBar(Widget childAppBar) {
   return PreferredSize(
     preferredSize: const Size.fromHeight(100.0), // chiều cao AppBar
@@ -26,7 +13,7 @@ PreferredSizeWidget getAppBar(Widget childAppBar) {
       ),
       clipBehavior: Clip.antiAlias,
       child: AppBar(
-        backgroundColor: ColorsTheme.primaryColor,
+        backgroundColor: AppColors.primaryColor,
         title: childAppBar,
         toolbarHeight: 100,
       ),
