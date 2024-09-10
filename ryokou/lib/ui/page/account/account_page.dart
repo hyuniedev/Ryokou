dd33fe00d1b9d74fa071d10c095fc3a04d662b7d
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2024-09-10 18:16:18 +0700

    + Pay screen

--- ryokou/lib/ui/page/account/account_page.dart
+++ ryokou/lib/ui/page/account/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ryokou/ui/booking/pay.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -10,6 +11,15 @@ class AccountPage extends StatefulWidget {
 class _AccountPage extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Pay()));
+            },
+            child: const Text('An em di')),
+      ),
+    );
   }
 }
