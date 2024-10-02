8a254b8fec0e5234e2a2120c30c3477ebdbf2db9
hyuniedev <huunien734@gmail.com>
2024-10-02 16:25:30 +0700

    + connect firebase to ryokou_service

--- ryokou_service/lib/main.dart
+++ ryokou_service/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ryokou_service/entity/tour.dart';
@@ -7,7 +8,9 @@ import 'package:ryokou_service/ui/page/listCustomer.dart';
 import 'package:ryokou_service/ui/page/listTour.dart';
 import 'package:ryokou_service/ui/page/newTour.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
