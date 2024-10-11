d8057c50936d2f0f72f168bae0597af7ad28e172
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2024-10-11 17:28:52 +0700

    +add slider

--- ryokou/lib/ui/sections/slider/slider.dart
+++ ryokou/lib/ui/sections/slider/slider.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:ryokou/themes/colors_theme.dart';
 
 class SliderBar extends StatefulWidget {
-  SliderBar({super.key, required this.numSlider});
-  int numSlider;
+  const SliderBar({super.key, required this.numSlider});
+  final int numSlider;
 
   @override
   State<SliderBar> createState() => _SliderBarState();
@@ -11,44 +11,60 @@ class SliderBar extends StatefulWidget {
 
 class _SliderBarState extends State<SliderBar> {
   int _curPage = 0;
-
   late PageController _pageController;
+
+  final List<String> imagePaths = [
+    'assets/image/slide1.png',
+    'assets/image/slide2.png',
+    'assets/image/slide3.png',
+    'assets/image/slide4.png',
+    'assets/image/slide5.png',
+    'assets/image/slide6.png',
+  ];
 
   @override
   void initState() {
     super.initState();
     _curPage = widget.numSlider ~/ 2;
-    _pageController =
-        PageController(initialPage: _curPage, viewportFraction: 0.8);
+    _pageController = PageController(
+      initialPage: _curPage,
+      viewportFraction: 0.8,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 150,
-        child: PageView.builder(
-          controller: _pageController,
-          onPageChanged: (value) {
-            setState(() {
-              _curPage = value;
-            });
-          },
-          itemCount: widget.numSlider,
-          itemBuilder: (context, index) {
-            return Transform.scale(
-              scaleY: _curPage == index ? 1.0 : 0.85,
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(color: AppColors.primaryColor, width: 2),
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Center(
-                  child: Text('Slider $index'),
+      height: 150,
+      child: PageView.builder(
+        controller: _pageController,
+        onPageChanged: (value) {
+          setState(() {
+            _curPage = value;
+          });
+        },
+        itemCount: imagePaths.length,
+        itemBuilder: (context, index) {
+          return Transform.scale(
+            scaleY: _curPage == index ? 1.0 : 0.85,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  imagePaths[index],
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
                 ),
               ),
-            );
-          },
-        ));
+            ),
+          );
+        },
+      ),
+    );
   }
 }
